52205aa42cb6ce3375bc0e37149c34707b613090
Manuel_Pepe <manuelpepe1995@gmail.com>
2018-10-02 21:14:25 +0200

    Consegna Manu_

---
+++ assignment4/manu_/src/LevelMaker.lua
@@ -0,0 +1,287 @@
+--[[
+    GD50
+    Super Mario Bros. Remake
+
+    -- LevelMaker Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+LevelMaker = Class{}
+
+function LevelMaker.generate(width, height, level)
+    local tiles = {}
+    local entities = {}
+    local objects = {}
+
+    local tileID = TILE_ID_GROUND
+
+    width = width + math.random(level) * 2
+    
+    -- whether we should draw our tiles with toppers
+    local topper = true
+    local tileset = math.random(20)
+    local topperset = math.random(20)
+    -- 
+    local lockBlockX = math.random(width)
+    local keyX = math.random(width)
+
+    while lockBlockX == keyX do
+        keyX = math.random(width)
+    end
+
+    local blockVariant = math.random(4)
+
+    -- insert blank tables into tiles for later access
+    for x = 1, height do
+        table.insert(tiles, {})
+    end
+
+    -- column by column generation instead of row; sometimes better for platformers
+    for x = 1, width do
+        local tileID = TILE_ID_EMPTY
+        
+        -- lay out the empty space
+        for y = 1, 6 do
+            table.insert(tiles[y],
+                Tile(x, y, tileID, nil, tileset, topperset))
+        end
+
+        -- chance to just be emptiness
+        if x ~= 1 and x ~= width and x ~= width - 1 and x ~= keyX and x ~= lockBlockX and math.random(7) == 1 then
+            for y = 7, height do
+                table.insert(tiles[y], 
+                Tile(x, y, tileID, nil, tileset, topperset))
+            end
+        else
+            tileID = TILE_ID_GROUND
+
+            local blockHeight = 4
+
+            for y = 7, height do
+                table.insert(tiles[y],
+                    Tile(x, y, tileID, y == 7 and topper or nil, tileset, topperset))
+            end
+
+            -- chance to generate a pillar
+            if math.random(8) == 1 and x ~= width and x ~= width - 1 then
+                blockHeight = 2
+                
+                -- chance to generate bush on pillar
+                if math.random(8) == 1 then
+                    table.insert(objects, blockMaker(x, y, blockHeight, objects))
+                end
+                
+                -- pillar tiles
+                tiles[5][x] = Tile(x, 5, tileID, topper, tileset, topperset)
+                tiles[6][x] = Tile(x, 6, tileID, nil, tileset, topperset)
+                tiles[7][x].topper = nil
+            
+            -- chance to generate bushes
+            elseif math.random(8) == 1 then
+                table.insert(objects, bushMaker(x, y, blockHeight, objects))
+            end
+
+            -- chance to spawn a block
+            if x ~= lockBlockX and x ~= width - 1 and math.random(10) == 1 then
+                table.insert(objects, blockMaker(x, y, blockHeight, objects))
+            elseif x == lockBlockX then
+                table.insert(objects, lockBlockMaker(x, y, blockHeight, objects, blockVariant, width))
+            elseif x == keyX then
+                table.insert(objects, keyMaker(x, y, blockHeight, objects, blockVariant))
+            end
+        end
+    end
+
+    local map = TileMap(width, height)
+    map.tiles = tiles
+    
+    return GameLevel(entities, objects, map)
+end
+
+
+function keyMaker(x, y, blockHeight, objects, variant)
+    return GameObject {
+        texture = 'keys-and-locks',
+        x = (x - 1) * TILE_SIZE,
+        y = (blockHeight - 1) * TILE_SIZE,
+        width = 16,
+        height = 16,
+
+        -- make it a random variant
+        frame = variant,
+        collidable = true,
+        consumable = true,
+        hit = false,
+        solid = false,
+
+        onConsume = function()
+            print("Key ")
+        end
+    }
+end
+
+function lockBlockMaker(x, y, blockHeight, objects, variant, width)
+    return GameObject {
+        texture = 'keys-and-locks',
+        x = (x - 1) * TILE_SIZE,
+        y = (blockHeight - 1) * TILE_SIZE,
+        width = 16,
+        height = 16,
+
+        -- make it a random variant
+        frame = 4 + variant,
+        collidable = true,
+        hit = false,
+        solid = true,
+
+        -- collision function takes itself
+        onCollide = function(obj)
+            local c = 0
+            local index = -1
+
+            for k, o in pairs(objects) do
+                if o == obj then
+                    index = k 
+                end
+
+                if o.texture == 'keys-and-locks' then
+                    c = c + 1
+                end
+            end
+
+            if c == 1 then
+                table.remove(objects, index)
+                    
+                table.insert(objects, poleMaker(width-1))
+                table.insert(objects, flagMaker(width))
+            end
+
+            gSounds['empty-block']:play()
+
+
+        end
+    }
+end
+
+function blockMaker(x, y, blockHeight, objects) 
+    -- jump block
+    return GameObject {
+        texture = 'jump-blocks',
+        x = (x - 1) * TILE_SIZE,
+        y = (blockHeight - 1) * TILE_SIZE,
+        width = 16,
+        height = 16,
+
+        -- make it a random variant
+        frame = math.random(#JUMP_BLOCKS),
+        collidable = true,
+        hit = false,
+        solid = true,
+
+        -- collision function takes itself
+        onCollide = function(obj)
+
+            -- spawn a gem if we haven't already hit the block
+            if not obj.hit then
+
+                -- chance to spawn gem, not guaranteed
+                if math.random(5) == 1 then
+
+                    -- maintain reference so we can set it to nil
+                    local gem = GameObject {
+                        texture = 'gems',
+                        x = (x - 1) * TILE_SIZE,
+                        y = (blockHeight - 1) * TILE_SIZE - 4,
+                        width = 16,
+                        height = 16,
+                        frame = math.random(#GEMS),
+                        collidable = true,
+                        consumable = true,
+                        solid = false,
+
+                        -- gem has its own function to add to the player's score
+                        onConsume = function(player, object)
+                            gSounds['pickup']:play()
+                            player.score = player.score + 100
+                        end
+                    }
+                    
+                    -- make the gem move up from the block and play a sound
+                    Timer.tween(0.1, {
+                        [gem] = {y = (blockHeight - 2) * TILE_SIZE}
+                    })
+                    gSounds['powerup-reveal']:play()
+
+                    table.insert(objects, gem)
+                end
+
+                obj.hit = true
+            end
+
+            gSounds['empty-block']:play()
+        end
+    }
+end
+
+function bushOnPillarMaker(x, y) 
+    return GameObject {
+        texture = 'bushes',
+        x = (x - 1) * TILE_SIZE,
+        y = (4 - 1) * TILE_SIZE,
+        width = 16,
+        height = 16,
+        
+        -- select random frame from bush_ids whitelist, then random row for variance
+        frame = BUSH_IDS[math.random(#BUSH_IDS)] + (math.random(4) - 1) * 7
+    }
+end
+
+function bushMaker(x, y)
+    return GameObject {
+        texture = 'bushes',
+        x = (x - 1) * TILE_SIZE,
+        y = (6 - 1) * TILE_SIZE,
+        width = 16,
+        height = 16,
+        frame = BUSH_IDS[math.random(#BUSH_IDS)] + (math.random(4) - 1) * 7,
+        collidable = false
+    }
+end
+
+function poleMaker(x)
+    local variant = math.random(6)
+    return GameObject {
+        texture = 'flags',
+        gframe = 'poles',
+        x = (x - 1) * TILE_SIZE,
+        y = 3 * TILE_SIZE,
+        width = 16,
+        height = 16,
+        frame = variant,
+        collidable = false
+    }
+end
+
+function flagMaker(x)
+    local variant = math.random(4)
+    print(variant)
+    return GameObject {
+        texture = 'flags',
+        gframe = 'flags',
+        x = (x - 3/2) * TILE_SIZE,
+        y = 3 * TILE_SIZE,
+        width = 16,
+        height = 16,
+        frame = 9*variant - 2,
+        collidable = true,
+        consumable = true,
+        hit = false,
+        solid = false,
+        onConsume = function(obj)
+            levelCompleted = true
+            print("level completed")
+        end
+    }
+end
