76a566b921e5271886fb95aa76d6d0cbb83902d3
Davide Cristini <davcri91@gmail.com>
2018-11-21 16:22:27 +0000

    Add davcri's assignment8

---
+++ assignment8/davcri/Assets/Resources/Scripts/AirplaneSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirplaneSpawner : MonoBehaviour {
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+
+		// trigger asynchronous randomized infinite spawning of airplanes
+		StartCoroutine(SpawnAirplanes());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnAirplanes() {
+		while (true) {
+
+			// instantiate a random airplane past the right egde of the screen, facing left
+			Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(7, 10), 11),
+				Quaternion.Euler(-90f, -90f, 0f));
+
+			// pause this coroutine for 3-10 seconds and then repeat loop
+			yield return new WaitForSeconds(Random.Range(3, 10));
+		}
+	}
+}
---
+++ assignment8/davcri/Assets/Resources/Scripts/HeliController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeliController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public int coinTotal = 0;
+	private Rigidbody rb;
+	private float vertical, horizontal;
+	public ParticleSystem explosion;
+	public AudioSource explosionSound;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// vertical axis is either up or down or w and s on the keyboard, among others
+		if (Input.GetAxisRaw("Vertical") != 0) {
+			vertical = Input.GetAxis("Vertical") * speed;
+
+			// constrain movement within the bounds of the camera
+			if (transform.position.y < -9.5f) {
+				transform.position = new Vector3(transform.position.x, -9.5f, transform.position.z);
+			}
+			if (transform.position.y > 9) {
+				transform.position = new Vector3(transform.position.x, 9, transform.position.z);
+			}
+		} else {
+			vertical = 0f;
+		}
+
+		// horizontal axis is either left or right or a and d on the keyboard, among others
+		if (Input.GetAxisRaw("Horizontal") != 0) {
+			horizontal = Input.GetAxis("Horizontal") * speed;
+
+			// constrain movement within the bounds of the camera
+			if (transform.position.x < -12.5f) {
+				transform.position = new Vector3(-12.5f, transform.position.y, transform.position.z);
+			}
+			if (transform.position.x > 15.5f) {
+				transform.position = new Vector3(15.5f, transform.position.y, transform.position.z);
+			}
+		}
+		else {
+			horizontal = 0f;
+		}
+
+		// set rigidbody's velocity to our input
+		rb.velocity = new Vector3(horizontal, vertical, 0);
+	}
+
+	public void PickupCoin() {
+		coinTotal += 1;
+
+		// trigger audio playback and emit particles from particle system
+		GetComponents<AudioSource>()[0].Play();
+		GetComponent<ParticleSystem>().Play();
+	}
+
+	public void PickupGem() {
+		coinTotal += 5;
+
+		// trigger audio playback and emit particles from particle system
+		GetComponents<AudioSource>()[0].Play();
+		GetComponent<ParticleSystem>().Play();
+	}
+
+	public void Explode() {
+		explosionSound.Play();
+
+		// set explosion position to helicopter's and emit
+		explosion.transform.position = transform.position;
+		explosion.Play();
+		
+		Destroy(gameObject);
+	}
+}
