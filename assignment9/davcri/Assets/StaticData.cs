3806811c16fba416fd1461951825eb545a46bc45
Davide Cristini <davcri91@gmail.com>
2018-12-07 10:02:35 +0000

    Add davcri's assignment 9

---
+++ assignment9/davcri/Assets/StaticData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticData {
+
+	private static int level;
+
+    public static int Level 
+    {
+        get 
+        {
+            return level;
+        }
+        set 
+        {
+            level = value;
+        }
+    }
+}
