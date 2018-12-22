68701abea46ad3d617f93ca4e9ebc28c639cc614
Davide Cristini <davcri91@gmail.com>
2018-12-22 10:44:23 +0100

    Add davcri's assignment10

---
+++ assignment10/davcri/Assets/Scripts/ValigettaScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ValigettaScript : MonoBehaviour {
+
+	public GameObject VictoryTextObject;
+	private VictoryScript victoryScript;
+
+	// Use this for initialization
+	void Start () {
+		victoryScript = VictoryTextObject.GetComponent<VictoryScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetType() == typeof(UnityEngine.CharacterController)) {
+			victoryScript.SetVictoryText();
+		}
+	}
+
+}
